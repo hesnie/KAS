33501526982d1d1baf04e996b5561e861cf95499
Henrik Søby Nielsen <hesnie@gmail.com>
2017-11-13 09:25:05 +0100

    classes created

---
+++ src/model/Location.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Location {
+
+    private String name;
+    private String adress;
+    private short maxParticipants;
+    private String description;
+
+    public Location() {
+
+    }
+}
