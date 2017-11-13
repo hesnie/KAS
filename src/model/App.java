74256d2542a22cacbea6637c1dd2b65357baa51e
Henrik Søby Nielsen <hesnie@gmail.com>
2017-11-13 16:00:32 +0100

    testing

--- src/model/App.java
+++ src/model/App.java
@@ -10,12 +10,8 @@ public class App {
         Service s1 = new Service();
         s1.initContent();
 
-<<<<<<< HEAD
         // s1.printTest();
-=======
         s1.printTest2();
-    }
->>>>>>> f513c62d1b47466fee45c42f17db4f14e34c89bd
 
         s1.printTest3();
     }
