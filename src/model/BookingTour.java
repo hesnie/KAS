03e4166b8be48eb9e1e61f8ea0214c42e7b0bac2
unknown <mathias.taankvist@gmail.com>
2017-11-13 10:30:56 +0100

    Companion updated

--- src/model/BookingTour.java
+++ src/model/BookingTour.java
@@ -1,5 +1,18 @@
 package model;
 
+import java.time.LocalDate;
+
 public class BookingTour {
+    private LocalDate date;
+    private Tour tour;
+
+    BookingTour(Tour tour, LocalDate date) {
+        this.date = date;
+        this.tour = tour;
+    }
+
+    Tour getTour() {
+        return tour;
+    }
 
 }
--- src/model/Companion.java
+++ src/model/Companion.java
@@ -1,10 +1,32 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Companion {
     private String name;
+    private ArrayList<BookingTour> bookingTours = new ArrayList<>();
 
     Companion(String name) {
         this.name = name;
+    }
+
+    BookingTour createBookingTour(Tour tour, LocalDate date) {
+        BookingTour b = new BookingTour(tour, date);
+        bookingTours.add(b);
+        return b;
+    }
+
+    float calcPrice() {
+        float price = 0;
+        for (int i = 0; i < bookingTours.size(); i++) {
+            price += bookingTours.get(i).getTour().getPrice();
+        }
+        return price;
+    }
+
+    String getName() {
+        return name;
     }
 
 }
