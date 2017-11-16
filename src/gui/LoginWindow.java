5afeeb49a9baac11e10b5ba14c1bd03d8d93e4a8
unknown <mathias.taankvist@gmail.com>
2017-11-16 16:25:56 +0100

    Gui admin del done

---
+++ src/gui/LoginWindow.java
@@ -0,0 +1,44 @@
+package gui;
+
+import model.service.Service;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class LoginWindow extends Stage {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public LoginWindow(String title) {
+        initStyle(StageStyle.UTILITY);
+        initModality(Modality.APPLICATION_MODAL);
+        setResizable(false);
+
+        setTitle(title);
+        BorderPane pane = new BorderPane();
+        initContent(pane);
+
+        Scene scene = new Scene(pane);
+        setScene(scene);
+    }
+
+private void initContent(GridPane pane) {
+        // show or hide grid lines
+        pane.setGridLinesVisible(false);
+
+        // set padding of the pane
+        pane.setPadding(new Insets(20));
+        // set horizontal gap between components
+        pane.setHgap(10);
+        // set vertical gap between components
+        pane.setVgap(10);
